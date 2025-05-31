98774b51bd470f459eb5b7f62fab4a0010a0cff1
Purplelaser92 <purplelaser92@gmail.com>
2025-05-30 19:13:47 -0600

    please work

---
+++ src/main/java/net/harry/harrysworkshop/HarrysWorkshop.java
@@ -0,0 +1,17 @@
+package net.harry.harrysworkshop;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HarrysWorkshop implements ModInitializer {
+	public static final String MOD_ID = "harrysworkshop";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/harry/harrysworkshop/HarrysWorkshopClient.java
@@ -0,0 +1,11 @@
+package net.harry.harrysworkshop;
+
+//important comment
+import net.fabricmc.api.ClientModInitializer;
+
+public class HarrysWorkshopClient implements ClientModInitializer {
+    @java.lang.Override
+    public void onInitializeClient() {
+
+    }
+}
