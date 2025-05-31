f466cb5275369c9ce033bfc27a329cdd5d59b7dc
Purplelaser92 <purplelaser92@gmail.com>
2025-05-30 20:12:32 -0600

    added pink spiral

--- src/main/java/net/harry/harrysworkshop/item/ModItems.java
+++ src/main/java/net/harry/harrysworkshop/item/ModItems.java
@@ -1,14 +1,16 @@
 package net.harry.harrysworkshop.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.harry.harrysworkshop.HarrysWorkshop;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
 
-    public static final Item PINK_SPIRAL = registerItem(name:"pink_spiral", new Item(new Item.Settings()));
+    public static final Item PINK_SPIRAL = registerItem("pink_spiral", new Item(new Item.Settings()));
 
     
     private static Item registerItem(String name, Item item) {
@@ -17,6 +19,10 @@ public class ModItems {
 
     public static void registerModItems() {
         HarrysWorkshop.LOGGER.info("Registering Mod Items for " + HarrysWorkshop.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(PINK_SPIRAL);
+        });
     }
 
 }
