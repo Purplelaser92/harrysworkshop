fee59e0672860ead4a85b35ee6c3f832feb0d4fb
Harrison Jolley <purplelaser92@gmail.com>
2025-06-14 12:32:48 -0600

    Recipe and loot table

--- src/main/java/net/harry/harrysworkshop/HarrysWorkshop.java
+++ src/main/java/net/harry/harrysworkshop/HarrysWorkshop.java
@@ -3,17 +3,18 @@ package net.harry.harrysworkshop;
 import net.fabricmc.api.ModInitializer;
 
 import net.harry.harrysworkshop.block.ModBlocks;
+import net.harry.harrysworkshop.item.ModItemGroups;
 import net.harry.harrysworkshop.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class HarrysWorkshop implements ModInitializer {
 	public static final String MOD_ID = "harrysworkshop";
-
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
 	}
---
+++ src/main/java/net/harry/harrysworkshop/item/ModItemGroups.java
@@ -0,0 +1,30 @@
+package net.harry.harrysworkshop.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.harry.harrysworkshop.HarrysWorkshop;
+import net.harry.harrysworkshop.block.ModBlocks;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroups {
+    public static final ItemGroup HARRYSWORKSHOP = Registry.register(Registries.ITEM_GROUP,
+            Identifier.of(HarrysWorkshop.MOD_ID, "harrysworkshop_items"),
+            FabricItemGroup.builder().icon(() -> new ItemStack(ModItems.PINK_SPIRAL))
+                    .displayName(Text.translatable("itemgroup.harrysworkshop.harrysworkshop_items"))
+                    .entries((displayContext, entries) -> {
+                        entries.add(ModItems.PINK_SPIRAL);
+                        entries.add(ModItems.VOID_ORB);
+                        entries.add(ModBlocks.VOID_BLOCK);
+
+                    })
+
+                    .build());
+
+    public static void registerItemGroups() {
+        HarrysWorkshop.LOGGER.info("Registering Item Groups for " + HarrysWorkshop.MOD_ID);
+    }
+}
